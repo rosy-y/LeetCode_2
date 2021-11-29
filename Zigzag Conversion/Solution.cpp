f38b676a194dbb79679a38994932284d9e882d7e
YeEun_Choi <89172314+rosy-y@users.noreply.github.com>
2021-11-29 20:22:52 +0900

    Update Solution.cpp
    6> Zigzag Conversion

--- Zigzag Conversion/Solution.cpp
+++ Zigzag Conversion/Solution.cpp
@@ -1,6 +1,20 @@
 class Solution {
 public:
     string convert(string s, int numRows) {
-        
+        if (numRows == 1) return s;
+
+        vector<string> v(min(numRows, int(s.size())));
+        int row = 0;
+        bool flag = false;
+
+        for (char c : s) {
+            v[row] += c;
+            if (row == 0 || row == numRows - 1) flag = !flag;
+            row += flag ? 1 : -1;
+        }
+
+        string ans;
+        for (string str : v) ans += str;
+        return ans;
     }
 };

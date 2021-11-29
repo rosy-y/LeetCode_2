1a3f5edb3e85d04202345760733bb8c484643956
YeEun_Choi <89172314+rosy-y@users.noreply.github.com>
2021-11-29 20:23:28 +0900

    Update Solution.cpp
    6> Reverse Integer

--- Reverse Integer/Solution.cpp
+++ Reverse Integer/Solution.cpp
@@ -1,6 +1,12 @@
 class Solution {
 public:
     int reverse(int x) {
+        long lo = 0;
         
+        while (x) {
+            lo = lo*10 + x%10;
+            x /= 10;
+        }
+        return (lo<INT_MIN || lo>INT_MAX) ? 0 : lo;
     }
 };

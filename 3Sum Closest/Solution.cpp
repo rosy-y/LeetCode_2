e8455d5a47d4826b19b3ba7f154a7f3f7ba4cb9f
YeEun_Choi <89172314+rosy-y@users.noreply.github.com>
2021-12-12 20:45:30 +0900

    Create Solution.cpp
    16> 3Sum Closest

---
+++ 3Sum Closest/Solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int threeSumClosest(vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        int sum = nums[0] + nums[1] + nums[n-1];        
+        
+        if (n == 3) return sum;
+        for (int i=0; i<n-2; i++) {
+            int j = i+1, k = n-1;
+            while (j < k) {
+                int tmp = nums[i] + nums[j] + nums[k];
+                if (tmp > target) k--;
+                else if (tmp == target) return target;
+                else j++;
+                if (abs(tmp-target) < abs(sum-target)) sum = tmp;
+            }            
+        }
+       return sum; 
+    }
+};

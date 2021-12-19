2561586679a596232c43767871ea76eca11bf8fb
YeEun_Choi <89172314+rosy-y@users.noreply.github.com>
2021-12-19 14:52:45 +0900

    Update Solution.cpp
    31> Next Permutation

--- Next Permutation/Solution.cpp
+++ Next Permutation/Solution.cpp
@@ -1,6 +1,13 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
+        int i = nums.size()-1, l = i;
+        for (i; i>0 && nums[i-1]>=nums[i]; i--); // 처음으로 감소하는 곳
         
+        if (i > 0) {
+            for (l; nums[l]<=nums[i-1]; l--);            
+            swap(nums[i-1], nums[l]);
+        }
+        reverse(nums.begin()+i, nums.end());
     }
 };

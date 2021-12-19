bc66728f66629d4334f43e5759a74381110e3b31
YeEun_Choi <89172314+rosy-y@users.noreply.github.com>
2021-12-19 14:24:06 +0900

    Update Solution.cpp
    29> Divide Two Integers

--- Divide Two Integers/Solution.cpp
+++ Divide Two Integers/Solution.cpp
@@ -1,6 +1,21 @@
 class Solution {
 public:
     int divide(int dividend, int divisor) {
+        if (dividend == INT_MIN && divisor == -1) return INT_MAX;
         
+        int sign = dividend>0 ^ divisor>0 ? -1 : 1;
+        long a = labs(dividend), b = labs(divisor), ans = 0;
+        
+        
+        while (a >= b) {
+            long tmp = b, n = 1;
+            while (tmp << 1 <= a) {
+                tmp <<= 1;
+                n <<= 1;
+            }
+            a -= tmp;
+            ans += n;
+        }
+        return sign * ans;
     }
 };

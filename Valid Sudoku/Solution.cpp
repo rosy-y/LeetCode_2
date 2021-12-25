ff4ed5a79a605710e2cd3e3a427035871249bdb3
YeEun_Choi <89172314+rosy-y@users.noreply.github.com>
2021-12-25 21:10:55 +0900

    Create Solution.cpp
    36> Valid Sudoku

---
+++ Valid Sudoku/Solution.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        for (int i=0; i<3; i++) {
+            if (i%3 == 0) {
+                
+            }
+        }
+    }
+};

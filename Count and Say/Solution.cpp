9508c1fa30174952b604cd090baf81a9bbb0e9ae
YeEun_Choi <89172314+rosy-y@users.noreply.github.com>
2021-12-26 21:15:39 +0900

    Update Solution.cpp
    38> Count and Say

--- Count and Say/Solution.cpp
+++ Count and Say/Solution.cpp
@@ -1,6 +1,15 @@
 class Solution {
 public:
-    string countAndSay(int n) {
+    string countAndSay(int n, string say="1") {
+        if (n == 1) return say;        
+        int i=0, s = say.size();
+        string str = "";
         
+        while (i < s) {
+            int j = i;
+            while (i<s && say[i]==say[j]) i++;
+            str += to_string(i-j) + say[j];
+        }  
+        return countAndSay(n-1, str);
     }
 };

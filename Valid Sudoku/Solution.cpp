2a3fa312be82ee47e550bfe5f4663d464d5dd228
YeEun_Choi <89172314+rosy-y@users.noreply.github.com>
2021-12-26 20:54:42 +0900

    Update Solution.cpp
    36> Valid Sudoku

--- Valid Sudoku/Solution.cpp
+++ Valid Sudoku/Solution.cpp
@@ -1,10 +1,19 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        for (int i=0; i<3; i++) {
-            if (i%3 == 0) {
-                
+        int row = board.size(), col = board[0].size();
+        int a1[9][9]={0}, a2[9][9]={0}, a3[9][9]={0};
+        
+        for (int i=0; i<row; i++) {
+            for (int j=0; j<col; j++) {
+                if (board[i][j] != '.') {
+                    int k = i/3*3 + j/3;  // 3*3 확인
+                    int n = board[i][j]-'0'-1;
+                    if (a1[i][n] || a2[j][n] || a3[k][n]) return false;
+                    a1[i][n] = a2[j][n] = a3[k][n] = 1;
+                }                
             }
         }
+        return true;
     }
 };

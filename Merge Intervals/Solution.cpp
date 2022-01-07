634ced6727eab8d71afde13a4cc0c53337ef4615
YeEun_Choi <89172314+rosy-y@users.noreply.github.com>
2022-01-07 10:22:14 +0900

    Create Solution.cpp
    56> Merge Intervals

---
+++ Merge Intervals/Solution.cpp
@@ -0,0 +1,12 @@
+ublic:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end());
+        vector<vector<int>> vv;
+        
+        for (auto i : intervals) {
+            if (vv.empty() || vv.back()[1] < i[0]) vv.push_back(i);
+            else vv.back()[1] = max(vv.back()[1], i[1]);
+        }
+        return vv;
+    }
+};

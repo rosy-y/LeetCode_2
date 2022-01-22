cd181b9665249404a508d950e93391e998b261a0
YeEun_Choi <89172314+rosy-y@users.noreply.github.com>
2022-01-22 19:34:25 +0900

    Create Solution.cpp
    89> Gray Code

---
+++ Gray Code/Solution.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<int> grayCode(int n) {
+        vector<int> v(1<<n);
+        
+        for (int i=0; i<(1<<n); i++) {
+            v[i] = i^(i>>1);
+        }
+        return v;
+    }
+};

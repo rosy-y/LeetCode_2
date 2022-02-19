27e52646ad90317dd95579037d1163bc52317457
YeEun_Choi <89172314+rosy-y@users.noreply.github.com>
2022-02-19 21:46:31 +0900

    Create Solution.cpp

---
+++ 0219/Solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+static bool comp(int a, int b) {
+return to_string(a)+to_string(b) > to_string(b)+to_string(a);
+}
+string largestNumber(vector<int>& nums) {
+string str = "";
+sort(nums.begin(), nums.end(), comp);
+for (auto n : nums) str += to_string(n);
+return str[0] == '0' ? "0" : str;
+}
+};

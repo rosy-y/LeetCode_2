3a8af02d07d9ec0db64348a92d258ec0c2c0055e
YeEun_Choi <89172314+rosy-y@users.noreply.github.com>
2022-03-02 23:31:22 +0900

    Update solution.cpp
    229> Majority Element II

--- Majority Element II/solution.cpp
+++ Majority Element II/solution.cpp
@@ -1,1 +1,19 @@
-031
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        int n = nums.size() / 3;
+        unordered_map<int, int> m;
+        vector<int> v;
+        
+        for (int a : nums) m[a]++;
+        
+        int ele = 0;
+        for (auto b : m) {
+            if (b.second > n) {
+                ele = b.first;
+                v.push_back(ele);
+            }
+        }
+        return v;
+    }
+};

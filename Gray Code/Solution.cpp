aa9763d11c5043dfb319781376a73d96f09f22c5
YeEun_Choi <89172314+rosy-y@users.noreply.github.com>
2022-03-07 23:30:25 +0900

    Update Solution.cpp

--- Gray Code/Solution.cpp
+++ Gray Code/Solution.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> grayCode(int n) {
-        vector<int> v(1<<n);
+        vector<int> v(1<<n);  // shift 참고
         
         for (int i=0; i<(1<<n); i++) {
             v[i] = i^(i>>1);

36ca07379a0051ac7dc7085ff71e7efba0839c39
YeEun_Choi <89172314+rosy-y@users.noreply.github.com>
2022-03-19 23:51:24 +0900

    Update Solution.cpp

--- Merge Intervals/Solution.cpp
+++ Merge Intervals/Solution.cpp
@@ -3,8 +3,8 @@ ublic:
         sort(intervals.begin(), intervals.end());
         vector<vector<int>> vv;
         
-        for (auto i : intervals) {
-            if (vv.empty() || vv.back()[1] < i[0]) vv.push_back(i);
+        for (auto i : intervals) {  // check
+            if (vv.empty() || vv.back()[1] < i[0]) vv.push_back(i);  
             else vv.back()[1] = max(vv.back()[1], i[1]);
         }
         return vv;

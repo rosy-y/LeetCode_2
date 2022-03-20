9b775d63a6480cf24fb46c30d56af19061f40bb8
YeEun_Choi <89172314+rosy-y@users.noreply.github.com>
2022-03-20 23:58:17 +0900

    Update Solution.cpp

--- 3Sum Closest/Solution.cpp
+++ 3Sum Closest/Solution.cpp
@@ -5,7 +5,7 @@ public:
         int n = nums.size();
         int sum = nums[0] + nums[1] + nums[n-1];        
         
-        if (n == 3) return sum;
+        if (n == 3) return sum;  // check
         for (int i=0; i<n-2; i++) {
             int j = i+1, k = n-1;
             while (j < k) {
